870913fae0e5c67b9ee4539fcc1c35abc7f1c446
Demo User <ubuntu@arm.localdomain>
2018-02-23 21:13:21 +0000

    commit

--- ROV_solo.py
+++ ROV_solo.py
@@ -4,16 +4,18 @@ import serial
 import time
 import re
 from shutil import copyfile
+from Adafruit_PCA9685 import PCA9685 as PWM
 
-ser = serial.Serial('/dev/ttyUSB0', 38400, timeout=0.15)
+#default address
+pwm = PWM(0x40)
 
+#set freq to 1000Hz
+pwm.set_pwm_freq(1000)
+
+ser = serial.Serial('/dev/ttyO4', 38400, timeout=0.15)
 time.sleep(5)
-
-#ser.flushInput()
-#ser.flushOutput()
+#ser.flushInput() ser.flushOutput()
 count = 0
-
-
 ####	backup .xml files	############
 mybackup = open("testfile5.xml",'r')
 backup = mybackup.read()
@@ -21,43 +23,33 @@ mybackup2 = open("testfile6.xml",'w')
 cover = mybackup2.write(backup)
 mybackup.close()
 mybackup2.close()
-############################################# 
-
-
+#############################################
 old_min = -1
 old_max = 1
-
 new_min = 500
 new_max = 1000
 
-temp = new_min	
+temp = new_min
 new_max = (new_max-new_min)/2
-new_min = 0	
+new_min = 0
 
 old_range = old_max - old_min
 new_range = new_max - new_min
 
+stopped=temp+new_max
 
-stopped=temp+new_max 
 threshold= new_max/2
-
-
-
 while True:
-
 	time.sleep(0.01)
-	
-#Just showing that packets are constantly coming in, even if the joystick isn't being pressed
- 
-#	count+=1
-#	print "PACKET # %i" % count
 
-########################	RECEIVING DATA		########################
-	
+#Just showing that packets are constantly coming in, even if the
+#joystick isn't being pressed
+
+#	count+=1 print "PACKET # %i" % count
+########################	RECEIVING DATA ########################
+
 	myfile = open("testfile4.xml",'w')
 	mydata = ser.readline()
-
-
 	if mydata:
 		mybackup=open('testfile6.xml','w')
 		mybackup.write(mydata)
@@ -67,44 +59,35 @@ while True:
 		mydata = mydatas.read()
 		mydatas.close()
 
-	
 	myfile.write(mydata)
-
 #	print mydata
-	
+
 ################################################################################
-###		PARSING DATA	################################################
-		
+###		PARSING DATA
+###		################################################
+
 	myfile.close()
-
 	time.sleep(0.001)
-
 	tree = ET.parse('testfile4.xml')
 	root = tree.getroot()
-
-#	print(float(root[0][0].text))	
-
+#	print(float(root[0][0].text))
 ################################################################################
-
-###########	MOTOR CALCULATIONS	########################################
-
+###########	MOTOR CALCULATIONS
+###########	########################################
 	lx = float(root[0][0].text)
 	ly = float(root[0][1].text)
 	rx = float(root[0][2].text)
 	ry = float(root[0][3].text)
-
 	newValueLX=int(((lx-old_min)*new_range)/old_range)+new_min
 	newValueLY=int(((ly-old_min)*new_range)/old_range)+new_min
 	newValueRX=int(((rx-old_min)*new_range)/old_range)+new_min
 	newValueRY=int(((ry-old_min)*new_range)/old_range)+new_min
-
 	motor1=stopped
 	motor2=stopped
 	motor3=stopped
 	motor4=stopped
 	motor5=stopped
 	motor6=stopped
-
 #	LEFT
 	if newValueLX > threshold:
 		leftval = newValueLX - threshold
@@ -112,7 +95,6 @@ while True:
 		motor2 += leftval
 		motor3 += leftval
 		motor4 -= leftval
-
 #	RIGHT
 	if newValueLX <= threshold:
 		rightval = threshold - newValueLX
@@ -120,7 +102,6 @@ while True:
 		motor2 -= rightval
 		motor3 -= rightval
 		motor4 += rightval
-
 #	FORWARD
 	if newValueLY > threshold:
 		fwdval = newValueLY - threshold
@@ -128,7 +109,6 @@ while True:
 		motor2 += fwdval
 		motor3 += fwdval
 		motor4 += fwdval
-
 #	BACKWARD
 	if newValueLY <= threshold:
 		backval = threshold - newValueLY
@@ -136,7 +116,6 @@ while True:
 		motor2 -= backval
 		motor3 -= backval
 		motor4 -= backval
-
 #	ROTATE LEFT
 	if newValueRX > threshold:
 		rotateleftval = newValueRX - threshold
@@ -144,7 +123,6 @@ while True:
 		motor2 += rotateleftval
 		motor3 -= rotateleftval
 		motor4 += rotateleftval
-
 #	ROTATE RIGHT
 	if newValueRX <= threshold:
 		rotaterightval = threshold - newValueRX
@@ -152,33 +130,27 @@ while True:
 		motor2 -= rotaterightval
 		motor3 += rotaterightval
 		motor4 -= rotaterightval
-
-#	ASCEND 
+#	ASCEND
 	if newValueRY > threshold:
 		ascendval = 2*(newValueRY - threshold)
 		motor5 += ascendval
 		motor6 += ascendval
-
 #	DESCEND
 	if newValueRY <= threshold:
 		descendval = 2*(threshold - newValueRY)
 		motor5 -= descendval
 		motor6 -= descendval
-	
+
+	pwm.set_pwm(0,0,motor1)
 
 	print "AXES:"
 	print "LX: %i" % newValueLX
 	print "LY: %i" % newValueLY
 	print "RX: %i" % newValueRX
 	print "RY: %i" % newValueRY
+#	motor1=	+800 motor2=	+800 motor3=	+800 motor4=	+800
+#	motor5=	+800 motor6=	+800
 
-#	motor1=	+800
-#	motor2=	+800
-#	motor3=	+800
-#	motor4=	+800
-#	motor5=	+800
-#	motor6=	+800
-	
 	print "MOTORS:"
 	print "1: %i" % motor1
 	print "2: %i" % motor2
@@ -186,5 +158,4 @@ while True:
 	print "4: %i" % motor4
 	print "5: %i" % motor5
 	print "6: %i" % motor6
-
 ###############################################################################
