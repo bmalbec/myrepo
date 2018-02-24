f6c0a17daa9e60a3823e8f63c6e3c55c09a4207d
tamucapstone <36775479+tamucapstone@users.noreply.github.com>
2018-02-24 15:37:16 -0600

    Update temp_test.py

--- temp_test.py
+++ temp_test.py
@@ -1,10 +1,31 @@
 import Adafruit_BBIO.ADC as ADC
 import time
+import sys
+import serial
+import struct
+import xml.etree.ElementTree as ET
+import math
+import signal
 
 tempPin = "AIN1"
 
+ser = serial.Serial('/dev/ttyO4', 38400)
+
 ADC.setup()
 time.sleep(0.01)
+
 while (True):
-  print(ADC.read(tempPin))
+  rawTemp = ADC.read(tempPin)
   time.sleep(2)
+  
+  tempData = ET.Element('data')
+    tempItem = ET.SubElement(tempData, 'temp')
+    tempItem1 = ET.SubElement(tempItem, 'item')
+    tempItem1.set('name','Temperature')
+    tempItem1.text = rawTemp
+  
+  tempXML = ET.tostring(tempData)
+    ser.write(tempXML)
+    ser.write('\n')
+  
+  print("Wrote ", rawTemp, " to UART\n"
