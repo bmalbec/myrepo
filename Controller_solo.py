dc2531fe00a3f93624cfd26ae98f43436b5c1a77
tamucapstone <36775479+tamucapstone@users.noreply.github.com>
2018-02-24 19:22:21 -0600

    Update Controller_solo.py

--- Controller_solo.py
+++ Controller_solo.py
@@ -7,12 +7,65 @@ import struct
 import xml.etree.ElementTree as ET
 import math
 import signal
+import time
+from US2066 import US2066Base as DISP
 
 #from xmodem import XMODEM
 from geometry_msgs.msg import Twist
 from sensor_msgs.msg import Joy
 from std_msgs.msg import String, Char, Float64, Int32
 #from Axes.msg import axes
+
+def oled_init(display):
+  display.command(0x01)
+  display.command(0x00)
+  display.write("Temperature:")
+  
+def oled_temp(display, temperature):
+  display.command(0xA0)
+  display.write(temperature)
+  
+def read_temp(tempOldSerialData):
+  tempXmlTemplate = open("temp_xml_template.xml", 'r')
+  tempXmlBackup = tempXmlTemplate.read()
+  tempXmlBackup2 = open("temp_xml_backup.xml", 'w+')
+  tempCover = tempXmlBackup2.write(tempXmlBackup)
+  tempXmlTemplate.close()
+  tempXmlBackup2.close()
+
+  tempXmlData = open("temp_xml_data.xml", 'w+')
+  tempSerialData = ser.readline()
+  if tempSerialData:
+    tempXmlBackup = open("temp_xml_backup.xml", 'w')
+    tempXmlBackup.write(tempSerialData)
+    tempOldSerialData = tempSerialData
+    tempXmlBackup.close()
+  if not tempSerialData:
+    tempSerialData = tempOldSerialData
+
+  tempXmlData.write(tempSerialData)
+
+  tempXmlData.close()
+  time.sleep(0.01)
+  tempXmlTree = ET.parse("temp_xml_data.xml")
+  tempXmlRoot = tempXmlTree.getroot()
+
+  temp = tempXmlRoot[0][0].text
+  
+return temp
+
+disp = DISP(0x3C)
+
+disp.begin()
+
+time.sleep(0.01)
+
+oled_init(disp)
+time.sleep(0.001)
+
+tempOldSerialInit = open("temp_xml_template.xml", 'r')
+tempOldSerialData = tempOldSerialInit.read()
+tempOldSerialInit.close()
 
 #axesArray = []
 axesArray = [1.0,1.0,1.0,1.0]
@@ -151,9 +204,9 @@ while True:
 		print "BUTTONS:"
 		print buttonArray
 
-
-
-
+		temp = read_temp(tempOldSerialData)
+		print(temp)
+		oled_temp(disp, temp)
 
 	def readXbox():
 		rospy.init_node('readXbox',anonymous=False)
