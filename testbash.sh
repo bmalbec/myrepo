e8e3deda6f90c664bde11325f0323dfd3f5c7730
tamucapstone <36775479+tamucapstone@users.noreply.github.com>
2018-02-25 14:27:24 -0600

    Update testbash.sh

--- testbash.sh
+++ testbash.sh
@@ -4,7 +4,7 @@
 
 cd ~
 cd catkin_ws/src/beginner_tutorials/scripts
-rm Controller_solo.py
+rm -f Controller_solo.py
 cd ~
 cp Controller_solo.py Controller_solo2.py
 mv Controller_solo.py catkin_ws/src/beginner_tutorials/scripts
