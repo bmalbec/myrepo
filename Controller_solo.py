3ee4f8283deed69ba7ec10023ccb2e3cf9b83ebc
tamucapstone <36775479+tamucapstone@users.noreply.github.com>
2018-02-25 13:37:37 -0600

    Update Controller_solo.py

--- Controller_solo.py
+++ Controller_solo.py
@@ -67,89 +67,90 @@ def read_temp(tempOldSerialData):
 
 #####	Populate the two arrays with data from the Xbox 360 controller	#####
 def callback(data):
-	while not rospy.is_shutdown():
-		#####	Initialize the arrays	#####
-		axesArray = []
-		buttonArray = []
+	
+	#####	Initialize the arrays	#####
+	axesArray = []
+	buttonArray = []
 
-		#####	Fill axesArray with the left analog stick's x & y axes, as well as the right analog stick's x & y axes, then cut off the arbitrary data	#####
-		axesArray.insert(0,data.axes[0])
-		axesArray.insert(1,data.axes[1])
-		axesArray.insert(2,data.axes[3])
-		axesArray.insert(3,data.axes[4])
-
-
-		#####	Fill buttonArray with the D-Pad's left/right/up/down data, then cut off the arbitrary data	#####
-		buttonArray.insert(0,data.buttons[11])
-		buttonArray.insert(1,data.buttons[12])
-		buttonArray.insert(2,data.buttons[13])
-		buttonArray.insert(3,data.buttons[14])
+	#####	Fill axesArray with the left analog stick's x & y axes, as well as the right analog stick's x & y axes, then cut off the arbitrary data	#####
+	axesArray.insert(0,data.axes[0])
+	axesArray.insert(1,data.axes[1])
+	axesArray.insert(2,data.axes[3])
+	axesArray.insert(3,data.axes[4])
 
 
-		#############################################
-		#####	Create the .xml structure	#####
-		#############################################
-
-		#####	Create the header (parent)	#####
-		controllerPacket = ET.Element('frame')
-
-		#####	Create the child, which will house all of the data	#####
-		controllerData = ET.SubElement(controllerPacket, 'data')
-
-		#####	Create spots for the analog stick values and D-Pad values, label them as "items"	#####
-		leftAnalogX = ET.SubElement(controllerData, 'item')
-		leftAnalogY = ET.SubElement(controllerData, 'item')
-		rightAnalogX = ET.SubElement(controllerData, 'item')
-		rightAnalogY = ET.SubElement(controllerData, 'item')
-		dPadLeft = ET.SubElement(controllerData, 'item')
-		dPadRight = ET.SubElement(controllerData, 'item')
-		dPadUp = ET.SubElement(controllerData, 'item')
-		dPadDown = ET.SubElement(controllerData, 'item')
-
-		#####	Assign names for each item to identify them easily	#####
-		leftAnalogX.set('name','Left_X')
-		leftAnalogY.set('name','Left_Y')
-		rightAnalogX.set('name','Right_X')
-		rightAnalogY.set('name','Right_Y')
-		dPadLeft.set('name','D_Left')
-		dPadRight.set('name','D_Right')
-		dPadUp.set('name','D_Up')
-		dPadDown.set('name','D_Down')
-
-		#####	Set the values of each item, type cast them to strings	#####
-		leftAnalogX.text = str(axesArray[0])
-		leftAnalogY.text = str(axesArray[1])
-		rightAnalogX.text = str(axesArray[2])
-		rightAnalogY.text = str(axesArray[3])
-		dPadUp.text = str(buttonArray[0])
-		dPadDown.text = str(buttonArray[1])
-		dPadLeft.text = str(buttonArray[2])
-		dPadRight.text = str(buttonArray[3])
-
-		#####	Package everything together, store it in the variable "myData"	#####
-		myData = ET.tostring(controllerPacket)
-
-		#####	Send info through the serial port, along with a newline character (required for ROV to read data properly)	#####
-		ser.write(myData)
-		ser.write('\n')
-
-		#####	Go to the function that reads the serial port for temperature data, set that data to the variable "temp"	#####
-	#	temp = read_temp(tempOldSerialData)
-
-		#####	Print the temperature value to the terminal (for debugging purposes, won't be visible in standard usage)	#####
-		#print(temp)
-
-		#####	Write the temperature (variable "temp") to the OLED Display (at I2C address "disp"), wait 10ms	#####
-	#	oled_temp(disp, temp)
-	#	time.sleep(0.01)
+	#####	Fill buttonArray with the D-Pad's left/right/up/down data, then cut off the arbitrary data	#####
+	buttonArray.insert(0,data.buttons[11])
+	buttonArray.insert(1,data.buttons[12])
+	buttonArray.insert(2,data.buttons[13])
+	buttonArray.insert(3,data.buttons[14])
 
 
-		#####	Print both arrays to the terminal (for debugging purposes, won't be visible in standard usage)	#####
-		print "AXES:"
-		print axesArray
-		print "BUTTONS:"
-		print buttonArray
+	#############################################
+	#####	Create the .xml structure	#####
+	#############################################
 
+	#####	Create the header (parent)	#####
+	controllerPacket = ET.Element('frame')
+
+	#####	Create the child, which will house all of the data	#####
+	controllerData = ET.SubElement(controllerPacket, 'data')
+
+	#####	Create spots for the analog stick values and D-Pad values, label them as "items"	#####
+	leftAnalogX = ET.SubElement(controllerData, 'item')
+	leftAnalogY = ET.SubElement(controllerData, 'item')
+	rightAnalogX = ET.SubElement(controllerData, 'item')
+	rightAnalogY = ET.SubElement(controllerData, 'item')
+	dPadLeft = ET.SubElement(controllerData, 'item')
+	dPadRight = ET.SubElement(controllerData, 'item')
+	dPadUp = ET.SubElement(controllerData, 'item')
+	dPadDown = ET.SubElement(controllerData, 'item')
+
+	#####	Assign names for each item to identify them easily	#####
+	leftAnalogX.set('name','Left_X')
+	leftAnalogY.set('name','Left_Y')
+	rightAnalogX.set('name','Right_X')
+	rightAnalogY.set('name','Right_Y')
+	dPadLeft.set('name','D_Left')
+	dPadRight.set('name','D_Right')
+	dPadUp.set('name','D_Up')
+	dPadDown.set('name','D_Down')
+
+	#####	Set the values of each item, type cast them to strings	#####
+	leftAnalogX.text = str(axesArray[0])
+	leftAnalogY.text = str(axesArray[1])
+	rightAnalogX.text = str(axesArray[2])
+	rightAnalogY.text = str(axesArray[3])
+	dPadUp.text = str(buttonArray[0])
+	dPadDown.text = str(buttonArray[1])
+	dPadLeft.text = str(buttonArray[2])
+	dPadRight.text = str(buttonArray[3])
+
+	#####	Package everything together, store it in the variable "myData"	#####
+	myData = ET.tostring(controllerPacket)
+
+	#####	Send info through the serial port, along with a newline character (required for ROV to read data properly)	#####
+	ser.write(myData)
+	ser.write('\n')
+
+	#####	Go to the function that reads the serial port for temperature data, set that data to the variable "temp"	#####
+#	temp = read_temp(tempOldSerialData)
+
+	#####	Print the temperature value to the terminal (for debugging purposes, won't be visible in standard usage)	#####
+	#print(temp)
+
+	#####	Write the temperature (variable "temp") to the OLED Display (at I2C address "disp"), wait 10ms	#####
+#	oled_temp(disp, temp)
+#	time.sleep(0.01)
+
+
+	#####	Print both arrays to the terminal (for debugging purposes, won't be visible in standard usage)	#####
+	print "AXES:"
+	print axesArray
+	print "BUTTONS:"
+	print buttonArray
+	
+	readXbox()
 
 #####	Read the data coming from the Xbox 360 controller, located at /dev/input/js0	#####
 def readXbox():
