b03ea94de1e5d155df1657380b5a76482b91e8ca
tamucapstone <36775479+tamucapstone@users.noreply.github.com>
2018-02-25 14:23:03 -0600

    Update testbash.sh

--- testbash.sh
+++ testbash.sh
@@ -2,7 +2,14 @@
 
 . ~/catkin_ws/devel/setup.bash
 
-roslaunch launchjoy.launch
 cd ~
-python Controller_solo.py
+cd catkin_ws/src/beginner_tutorials/scripts
+rm Controller_solo.py
+cd ~
+cp Controller_solo.py Controller_solo2.py
+mv Controller_solo.py catkin_ws/src/beginner_tutorials/scripts
+mv Controller_solo2.py Controller_solo.py
+
+roslaunch launchjoy.launch
+
 
