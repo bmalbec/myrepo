5b326e9bff9806c8aa636fac7c3a3cde76c13126
tamucapstone <36775479+tamucapstone@users.noreply.github.com>
2018-02-25 10:32:14 -0600

    Update Controller_solo.py

--- Controller_solo.py
+++ Controller_solo.py
@@ -8,13 +8,11 @@ import xml.etree.ElementTree as ET
 import math
 import signal
 import time
-from US2066 import US2066Base as DISP
 
-#from xmodem import XMODEM
+from US2066 import US2066Base as DISP
 from geometry_msgs.msg import Twist
 from sensor_msgs.msg import Joy
 from std_msgs.msg import String, Char, Float64, Int32
-#from Axes.msg import axes
 
 def oled_init(display):
   display.command(0x01)
@@ -71,7 +69,6 @@ time.sleep(0.01)
 tempOldSerialInit.close()
 time.sleep(0.01)
 
-#axesArray = []
 axesArray = [1.0,1.0,1.0,1.0]
 buttonArray = [1.0, 1.0, 1.0, 1.0]
 count=4
@@ -81,28 +78,9 @@ ser = serial.Serial('/dev/ttyO4', 38400)
 while True:
 	def callback(data):
 
-		#ser = serial.Serial('/dev/ttyO4', 38400)
-	#	ser.flush()
-	#	ser.flushInput()
-	#	ser.flushOutput()
-
-#		item="[Left analog x: %.3f]" % data.axes[0]
-#		item2="[Left analog y: %.3f]" % data.axes[1]
-#		item3="[Right analog x: %.3f]" % data.axes[3]
-#		item4="[Right analog y: %.3f]" % data.axes[4]
-#		total=item+item2+item3+item4
-		#axes.insert(data.axes[0],data.axes[1])#,data.axes[3],data.axes[4])
-		#axes.append(data.axes[3])
-		#axes.append(data.axes[4])
-		#axes=[data.axes[0],data.axes[1],data.axes[3],data.axes[4]]
-
-		#axesArray[1]=data.axes[0]
-		#axesArray[2]=data.axes[1]
-		#axesArray[3]=data.axes[3]
-		#axesArray[4]=data.axes[4]
 		axesArray = [1.0,1.0,1.0,1.0]
 		buttonArray = [1.0, 1.0, 1.0, 1.0]
-####	Method 1 (which works totally fine)	###########################
+
 
 		axesArray.insert(0,data.axes[0])
 		axesArray.insert(1,data.axes[1])
@@ -115,41 +93,7 @@ while True:
 		buttonArray.insert(2,data.buttons[13])
 		buttonArray.insert(3,data.buttons[14])
 		buttonArray=buttonArray[:-4]
-#################################################
 
-
-
-####	Method 2	###########################
-#	bumplx = float(data.axes[0])+1
-#	bumply = float(data.axes[1])+1
-#	bumprx = float(data.axes[3])+1
-#	bumpry = float(data.axes[4])+1
-#	lx = "%.5f" % bumplx
-#	ly = "%.5f" % bumply
-#	rx = "%.5f" % bumprx
-#	ry = "%.5f" % bumpry
-#	axesArray.insert(0,lx)
-#	axesArray.insert(1,ly)
-#	axesArray.insert(2,rx)
-#	axesArray.insert(3,ry)
-#	axesArray=axesArray[:-4]
-###	testing with .txt files		###
-#	myfile = open("xboxAxes.txt","w")
-#	myfile.write(axesArray)
-#	print axesArray
-
-#################################################
-
-#	sys.stdout.write("\r"+total)
-	#for i in range(count):
-	#test=ser.write(str(axesArray))
-#	test=ser.write('\n')
-#	sys.stdout.flush()
-	#print axesArray
-
-
-	# create the file structure
-####	temporarily blocked out so i can test .txt files	####
 		xdata = ET.Element('data')
 		xitems = ET.SubElement(xdata, 'axes')
 		xitem1 = ET.SubElement(xitems, 'item')
@@ -177,33 +121,10 @@ while True:
 		xitem7.text = str(buttonArray[2])
 		xitem8.text = str(buttonArray[3])
 
-
-
-
-	# create a new XML file with the results
 		mydata = ET.tostring(xdata)
 		ser.write(mydata)
 		ser.write('\n')
-#	def getc(size, timeout=0.1):
-#		return ser.read(size)
-#	def putc(xdata, timeout=0.1):
-#		ser.write(xdata)
-#	modem = XMODEM(getc, putc)
 
-#################	this stuff works temporarily	##############
-	#	myfile = open("xboxAxes.xml", "w")
-#	myfile = open('xboxAxes.xml','rb')
-#	modem.send(myfile)
-	#	myfile.write(mydata)
-#	myfile.write('\n')
-######################################################################
-	#	ser.write(myfile)
-		#ser.write(mydata)
-		#ser.write('\n')
-#		myfile.close()
-
-
-#	print test
 		print "AXES:"
 		print axesArray
 		print "BUTTONS:"
@@ -220,12 +141,6 @@ while True:
 
 		pubAxes = rospy.Publisher("axes",String,queue_size=10)
 
-	#ser = serial.Serial('/dev/ttyUSB0', 38400)
-	#test=ser.write('test')
-	#print test
-	#pubAxes.publish(axesArray)
-	#pubAxes.publish(test)
-	#ser.write("test")
 		rospy.spin()
 
 	if __name__=='__main__':
