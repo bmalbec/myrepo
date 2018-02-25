f3617606dc487bf6afcb632f66a0aa760e84036c
tamucapstone <36775479+tamucapstone@users.noreply.github.com>
2018-02-25 13:41:41 -0600

    Update Controller_solo.py

--- Controller_solo.py
+++ Controller_solo.py
@@ -161,7 +161,7 @@ def readXbox():
 	print "readXbox prior rospy.subscriber"
 	#####	Subscribe to the "joy" topic, which uses message type "Joy", and set the data to the variable "callback"	#####	
 	rospy.Subscriber("joy",Joy,callback)
-	
+	print "readXbox post rospy.subscriber"
 	#####	Create a topic called "axes" for other nodes to read the custom data packet (won't be used, since no other node is talking to it)	#####
 	#pubAxes = rospy.Publisher("axes",String,queue_size=10)
 
