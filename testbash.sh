78084d22caf97daa6849d8a43c1c1c70eda695ab
tamucapstone <36775479+tamucapstone@users.noreply.github.com>
2018-02-27 16:05:30 -0600

    Update testbash.sh

--- testbash.sh
+++ testbash.sh
@@ -5,7 +5,7 @@
 cd ~/catkin_ws/src/beginner_tutorials/scripts
 rm -f Controller_solo_dynamic.py temp_xml_template.xml
 cd ~
-cp Controller_solo_dynamic.py temp_xml_template.xml catkin_ws/src/beginner_tutorials/scripts
+cp Controller_solo_dynamic.py catkin_ws/src/beginner_tutorials/scripts
 cd ~/catkin_ws/src/beginner_tutorials/scripts
 chmod +x Controller_solo_dynamic.py
 cd ~
