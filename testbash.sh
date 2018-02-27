bccfd96076fef9366468335713b70673617be68c
tamucapstone <36775479+tamucapstone@users.noreply.github.com>
2018-02-27 10:58:29 -0600

    Update testbash.sh

--- testbash.sh
+++ testbash.sh
@@ -6,6 +6,9 @@ cd ~/catkin_ws/src/beginner_tutorials/scripts
 rm -f Controller_solo_dynamic.py temp_xml_template.xml
 cd ~
 cp Controller_solo_dynamic.py temp_xml_template.xml catkin_ws/src/beginner_tutorials/scripts
+cd ~/catkin_ws/src/beginner_tutorials/scripts
+chmod +x Controller_solo_dynamic.py
+cd ~
 
 roslaunch launchjoy.launch
 
