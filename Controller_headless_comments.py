13dec0b9a2a750b063da94133b8493adb54c2e2c
tamucapstone <36775479+tamucapstone@users.noreply.github.com>
2018-03-26 12:55:08 -0500

    Update Controller_headless_comments.py

--- Controller_headless_comments.py
+++ Controller_headless_comments.py
@@ -94,11 +94,17 @@ def read_temp():
 
 	time.sleep(0.001)
 	if tempSerialData:
+		#####	Testing March 26, 2018	######
+		receivedData = 1
+		######################################
 		tempXmlBackup = open('/home/ubuntu/temp_xml_backup.xml', 'w+')
 		tempXmlBackup.write(tempSerialData)
 		tempOldSerialData = tempSerialData
 		tempXmlBackup.close()
 	if not tempSerialData:
+		#####	Testing March 26, 2018	######
+		receivedData = 0
+		######################################
 		tempXmlBackup = open('/home/ubuntu/temp_xml_backup.xml', 'r')
 		tempBackupData = tempXmlBackup.read()
 		tempSerialData = tempBackupData
@@ -118,6 +124,21 @@ def read_temp():
 
 	return temp
 
+#####	Testing March 26, 2018 by Brian Malbec	#####
+def check_status(display,receivedData):
+	display.command(0x01)
+	display.command(0x00)
+#	display.write("Controller: ")
+#	
+#	display.command(0xA0)
+	display.write("ROV: ")
+	if receivedData == 1:
+		display.write("Up")
+	if receivedData == 0:
+		display.write("Down")
+	display.command(0x0C)
+	
+		
 #####	Populate the two arrays with data from the Xbox 360 controller	#####
 def callback(data):
 
@@ -193,9 +214,20 @@ def callback(data):
 	#####	Print the temperature value to the terminal (for debugging purposes, won't be visible in standard usage)	#####
 #	print(temp)
 
+
+	#####	Testing as of March 26, 2018 by Brian Malbec	#####
+	if (data.buttons[0] == 0) && (screenChangeFlag == 1):
+		screenChangeFlag = 0
+		oled_init(disp)
+		time.sleep(0.001)
+
 	#####	Write the temperature (variable "temp") to the OLED Display (at I2C address "disp"), wait 10ms	#####
-	oled_temp(disp, temp)
-	time.sleep(0.001)
+#	oled_temp(disp, temp)
+#	time.sleep(0.001)
+	#####	Testing as of March 26, 2018 by Brian Malbec	#####
+	if (data.buttons[0] == 0) && (screenChangeFlag == 0):
+		oled_temp(disp, temp)
+		time.sleep(0.001)
 
 
 	#####	Print both arrays to the terminal (for debugging purposes, won't be visible in standard usage)	#####
@@ -206,6 +238,10 @@ def callback(data):
 #	print "BUTTONS:"
 #	print buttonArray
 
+	#####	Testing as of March 26, 2018 by Brian Malbec	#####
+	if data.buttons[0] == 1:
+		screenChangeFlag = 1
+		check_status(disp,receivedData)
 
 
 #####	Read the data coming from the Xbox 360 controller, located at /dev/input/js0	#####
@@ -255,6 +291,8 @@ time.sleep(0.001)
 init_temp_values('/home/ubuntu/temp_xml_template.xml')
 time.sleep(0.001)
 
+screenChangeFlag = 0
+receivedData = 0
 
 #############################
 #####	Main loop	#####
